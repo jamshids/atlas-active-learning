ee27e3de84ef165545d6a44a0dd617e1c0a2445c
Jamshid Sourati <jamshid.sourati@childrens.harvard.edu>
2017-08-01 16:22:54 -0400

    Adding a function for writing non-overlapped informative patches into a text file

--- prep_dat.py
+++ prep_dat.py
@@ -55,3 +55,62 @@ def gen_batch_tensors(dat, batch_inds):
         batches += [batch_dat]
     
     return batches
+
+def save_patches(img, s, out_addr):
+    """Writing patches of an image into a text file, such that each 
+    patch is saved as one row of the file (after vectorization)
+    
+    Input variables include the input 3D image, size of each patch
+    (as a list of 3 elements), and name of the output file
+    """
+    
+    (xs,ys,zs) = img.shape
+    x_pnum = int(xs / s[0])
+    y_pnum = int(ys / s[1])
+    z_pnum = int(zs / s[2])
+
+    # information threshold
+    info_thr = 2.
+
+    # saving the first image
+    with open(out_addr,"w") as f:
+        for i in range(x_pnum):
+            for j in range(y_pnum):
+                for k in range(z_pnum):
+
+                    # 5x5x5 patches
+                    # preparing the ranges:
+                    # x
+                    if i<x_pnum-1:
+                        x_rng = [i*s[0] , (i+1)*s[0]]
+                    else:
+                        x_rng = [i*s[0] , xs]
+                    # y
+                    if j<y_pnum-1:
+                        y_rng = [j*s[1] , (j+1)*s[1]]
+                    else:
+                        y_rng = [j*s[1] , ys]
+                    # z
+                    if k<z_pnum-1:
+                        z_rng = [k*s[2] , (k+1)*s[2]]
+                    else:
+                        z_rng = [k*s[2] , zs]
+
+                    patch = img[x_rng[0]:x_rng[1],
+                                y_rng[0]:y_rng[1],
+                                z_rng[0]:z_rng[1]]
+
+                    # reshaping the patch to a row vector
+                    patch = np.reshape(patch, np.prod(patch.shape))
+
+                    # how much information the patch has
+                    #info_metric = np.std(patch)
+                    patch_hist, b_edges = np.histogram(patch, bins=20, density=True)
+                    w = b_edges[1] - b_edges[0]
+                    patch_hist *= w
+                    patch_hist[patch_hist==0] += 1e-6
+                    info_metric = -sum(patch_hist*np.log(patch_hist))
+
+                    # save the patch only if it has sufficient info
+                    if info_metric > info_thr:
+                        f.write("".join(" ".join(map(str, x)) for x in (patch,))+'\n')
