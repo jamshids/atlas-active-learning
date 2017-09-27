4c6b2a622b31427ba7775f0ea9c241a1ba13e63e
Jamshid Sourati <jamshid.sourati@childrens.harvard.edu>
2017-09-27 12:32:30 -0400

    Computing CC between images using Yao's C module; and saving them into .txt file

---
+++ compute_cc.sh
@@ -0,0 +1,43 @@
+MYDIR="/common/external/rawabd/Mindboggle-101/NRRD/"
+RESULTDIR="/common/external/rawabd/Mindboggle-101/CC/Results"
+
+# listing all the files in an array
+fnames=(/common/external/rawabd/Mindboggle-101/NRRD/*)
+printf "%s\n" "${fnames[@]}" > results/image_list.txt
+
+
+for i in ${!fnames[@]}
+do 
+    # writing the file if necessary
+    if [ ! -e "results/$i.txt" ] ; then
+	touch "results/$i.txt"
+	
+	# put 100 empty line in the file
+	for t in {0..99}; do echo -en '\n' >> results/$i.txt; done
+    fi
+
+    for ((j=$i+1;j<=${#fnames[@]};j++))
+    do
+	# do not re-compute cross-correlations if it is already computed
+	if [ -e "results/details/cc_$i-$j.txt" ] ; then
+	    cc=$( tail -n 1 results/details/cc_$i-$j.txt )
+	    # writing it in its location
+	    sed -i "${j}i${cc}" results/$i.txt
+	    continue
+	fi
+	
+	echo ""
+	echo "*******************************************************************"
+	echo "Computing cross-correlations between image $i and $j"
+	echo "image 1: ${fnames[$i]}"
+	echo "image 2: ${fnames[$j]}"
+	echo "*******************************************************************"
+	echo ""
+
+	./lcc ${fnames[$i]} ${fnames[$j]} 2 2 2 1 10 5
+	
+	mv lcc.txt results/details/cc_$i-$j.txt
+	mv index.txt results/details/index_$i-$j.txt
+	
+    done
+done
