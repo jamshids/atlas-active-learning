25b1504d54e88138f2a0181bf9b6f7bf67504c44
Jamshid Sourati <jamshid.sourati@childrens.harvard.edu>
2017-10-25 12:19:50 -0400

    Similarity matrix based on local CCs

--- utilities.py
+++ utilities.py
@@ -29,3 +29,62 @@ def pairwise_masked_CC(cc_path, index_path, mask):
         cc_map[coords[0],coords[1],coords[2]] = CC[row_idx]
         
     return cc_map
+
+def local_CC_similarity(data_obj, cc_dir,indices_dir,LOI, start_ID):
+    """Computing a similarity matrix based on local cross-correlations
+    
+    *Parameters:*
+    
+      data_obj: object of Mindboggle_101 class
+        make sure to run data_obj.list_data_path() before calling this
+        function, so that the paths to all images and masks are listed
+      
+      cc_dir: string
+        directory path where the cross-correlation files exists; these
+        include values of cross-correlation of the pactches in each
+        image versus another
+        
+     indices_dir: string
+       directory path to the folder where indices of non-ingored patches
+       are store; for each pair of images we have one such a file which
+       specifies the patches that are non-homogeneous enough to take
+       part in computing cross-correlation between that pair of images
+       
+     masks: 3D array
+          label map of the first image 
+          
+     LOI: list of integers
+       labels of interest
+    """
+    
+    n_samples = 100
+
+    #S = np.zeros((n_samples,n_samples))
+    S = np.loadtxt('Heschle_gyrus_sim.txt')
+    for i in range(start_ID, n_samples):
+        S[i,i] = 1.
+        if i==n_samples-1:
+            break
+        
+        # reading mask of the first image
+        seg_path = data_obj.segs_path[i]
+        seg, options = nrrd.read(seg_path)
+        
+        # keeping only the labels of interest
+        mask = np.zeros(seg.shape, dtype=bool)
+        for label in LOI:
+            mask = np.logical_or(mask, seg==label)
+        
+        # reading the CC map between the i-th image and others
+        for j in range(i+1, n_samples):
+            cc_path = '%s/cc_%d-%d.txt'% (cc_dir, i, j)
+            index_path = '%s/index_%d-%d.txt'% (indices_dir,  i, j)
+            
+            cc_map = pairwise_masked_CC(cc_path, index_path, mask)
+            S[i,j] = np.mean(cc_map[cc_map>0])
+            S[j,i] = S[i,j]
+            
+            print("(%d, %d)"% (i,j), end=', ')
+            np.savetxt('Heschle_gyrus_sim_2.txt', S)
+    return S
+            
